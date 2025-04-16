7522f9687a2cb2a938ffa3e804dadefc435f12e7
fineswift <gjrhkdgh3@naver.com>
2025-04-16 11:06:40 +0900

    Initial commit

---
+++ CustomAlert/CustomAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAlertApp.swift
+//  CustomAlert
+//
+//  Created by 허광호 on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
